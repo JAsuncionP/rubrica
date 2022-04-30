1a399036dd875189a1a0ea1493e8963fb873b051
omendoza1 <99977175+omendoza1@users.noreply.github.com>
2022-04-29 19:27:27 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Footer from "./components/Footer";
+import UserCrud from "./components/UserCrud";
+
+function App() {
+  return (
+    <div className="App">
+      <UserCrud/>
+      <Footer/>
+      
+    </div >
+  );
+}
+
+export default App;
